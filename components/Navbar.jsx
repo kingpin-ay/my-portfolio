5bc1b1b04d747293d109c4f4f00b280ed0b95cd9
kingpin-ay <ayushmondal001@gmail.com>
2022-10-19 20:28:37 +0530

    Adding the Blog site link to the website and also adding the youtube link to the website

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,23 +1,26 @@
-import React from 'react'
-import { BsFillMoonStarsFill} from "react-icons/bs";
+import React from "react";
+import { BsFillMoonStarsFill } from "react-icons/bs";
 
 export default function Navbar(props) {
   return (
     <nav className="py-10 mb-12 flex justify-between">
-            <h1 className="text-xl font-burtons dark:text-yellow-100">developedby</h1>
-            <ul className="flex items-center">
-              <li>
-                <BsFillMoonStarsFill onClick={() => props.setDarkMode(!props.darkMode)} className="cursor-pointer text-2xl" />
-              </li>
-              <li >
-                <a
-                  className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8 dark:text-white"
-                  href="#"
-                >
-                  Resume
-                </a>
-              </li>
-            </ul>
+      <h1 className="text-xl font-burtons dark:text-yellow-100">developedby</h1>
+      <ul className="flex items-center">
+        <li>
+          <BsFillMoonStarsFill
+            onClick={() => props.setDarkMode(!props.darkMode)}
+            className="cursor-pointer text-2xl"
+          />
+        </li>
+        <li>
+          <a
+            className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8 dark:text-white"
+            href="https://ayush-blogs.digitalpress.blog/"
+          >
+            Blog
+          </a>
+        </li>
+      </ul>
     </nav>
-  )
+  );
 }
